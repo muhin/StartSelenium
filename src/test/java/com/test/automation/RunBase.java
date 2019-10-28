e2c4338415a9d73ac75b8f381b61f20db76c0357
Manzumul Haque <manzumul@metatude.com>
2019-10-28 17:25:36 +0600

    refactored

--- src/test/java/com/test/automation/RunBase.java
+++ src/test/java/com/test/automation/RunBase.java
@@ -3,21 +3,26 @@ package com.test.automation;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class RunBase {
+public class RunBase
+{
 	protected static WebDriver driver;
-	
-	protected void setup() {
+
+	protected void setup()
+	{
 		driver = getWebDriver();
 	}
-	protected void tearDown() {
-		if(driver !=null)
+
+	protected void tearDown()
+	{
+		if (driver != null)
 			driver.close();
 	}
-	protected WebDriver getWebDriver() {
+
+	protected WebDriver getWebDriver()
+	{
 		System.setProperty("webdriver.chrome.driver", "D:\\git\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		return driver;
 	}
-
 }
