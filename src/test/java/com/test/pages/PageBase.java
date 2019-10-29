e6e852a1c2829b06807c5910cce78c1f6251f612
Manzumul Haque <manzumul@metatude.com>
2019-10-29 14:36:45 +0600

    refactored

--- src/test/java/com/test/pages/PageBase.java
+++ src/test/java/com/test/pages/PageBase.java
@@ -1,6 +1,17 @@
 package com.test.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class PageBase
 {
+	protected WebDriver driver;
+	protected WebDriverWait wait;
 
+	public PageBase(final WebDriver driver)
+	{
+		super();
+		this.driver = driver;
+		wait = new WebDriverWait(driver, 30);
+	}
 }
